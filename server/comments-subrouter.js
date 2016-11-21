f6497513d07caef80c7a2f142aac1725fac46939
Evan DiGiambattista <edigiam@me.com>
2016-11-20 20:28:33 -0500

    Finished with help from solutions :(

--- server/comments-subrouter.js
+++ server/comments-subrouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({
-    mergeParams: true // so these routes can receive "secretId"
+  mergeParams: true // so these routes can receive "secretId"
 });
 module.exports = router;
 
@@ -8,4 +8,26 @@ const models = require('../db/models');
 const Secret = models.Secret;
 const Comment = models.Comment;
 
-router.post('/', function (req, res, next) {});
+router.get('/', function(req, res, next) {
+
+  Secret.findById(+req.params.secretId, { include: [Comment] })
+    .then(function(foundSecret) {
+      res.send(foundSecret.comments);
+    })
+    .catch(next);
+
+});
+
+
+router.post('/', function(req, res, next) {
+  // add comment to database
+  Comment.create({
+      text: req.body.text,
+      secretId: req.params.secretId
+    })
+    // rerender page?
+    .then(function() {
+      res.redirect(`/secrets/${req.params.secretId}`);
+    })
+    .catch(next);
+});
--- server/secrets-subrouter.js
+++ server/secrets-subrouter.js
@@ -5,13 +5,61 @@ module.exports = router;
 const models = require('../db/models');
 const Secret = models.Secret;
 const Comment = models.Comment;
+const COUNT_PER_PAGE = 5;
 
-router.get('/', function (req, res, next) {});
+router.get('/', function(req, res, next) {
+  req.query.page = req.query.page || 1;
 
-router.get('/add', function (req, res, next) {});
+  const countingTotalPages = Secret.count().then(function(amountOfAllPages) {
+    return Math.ceil(amountOfAllPages / COUNT_PER_PAGE);
+  });
 
-router.get('/:secretId', function (req, res, next) {});
+  const queryConfig = {
+    limit: COUNT_PER_PAGE,
+    offset: (req.query.page - 1) * COUNT_PER_PAGE,
+    order: 'id DESC'
+  };
 
-router.post('/', function (req, res, next) {});
+  const findingSecretsForThisPage = Secret.findAll(queryConfig)
+
+  Promise.all([findingSecretsForThisPage, countingTotalPages])
+    .then(function(values) {
+      console.log(values)
+      const secrets = values[0];
+      const totalPages = values[1];
+      res.render('index', {
+        secrets: secrets,
+        page: req.query.page,
+        totalPages: totalPages
+      });
+    })
+    .catch(next);
+});
+
+router.get('/add', function(req, res, next) {
+  res.render('add');
+});
+
+router.get('/:secretId', function(req, res, next) {
+	// Where does include obj come from?
+  Secret.findById(req.params.secretId, { include: [Comment] })
+    .then(function(foundSecret) {
+    	// Where does this obj come from?
+      res.render('secret', { secret: foundSecret });
+    })
+    .catch(next);
+
+});
+
+router.post('/', function(req, res, next) {
+  // add text to secret database
+  Secret.create({
+      text: req.body.text
+    })
+    // redirect to '/secrets'
+    .then(function() {
+      res.redirect('/secrets');
+    })
+});
 
 router.use('/:secretId/comments', require('./comments-subrouter'));
